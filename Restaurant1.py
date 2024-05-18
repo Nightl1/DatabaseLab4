8d4753dc984bf83a292052fd22500b444528609a
Murtaza Haidary <LE1983326@crc-lennox.qc.ca>
2024-05-17 21:09:26 -0400

    restaurants part 1done

--- Restaurant1.py
+++ Restaurant1.py
@@ -1,22 +1,49 @@
+from pymongo import MongoClient
 import pandas as pd
 
-# Read CSV file
-df = pd.read_csv('sample_restaurants.restaurants.csv')
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['DatabaseLab4']  
+restaurants_collection = db['Restaurants']  
 
-# Create DF for Polish restaurants
-polish_restaurants = df[df['cuisine'] == 'Polish']
-polish_grouped = polish_restaurants.groupby('borough').size().reset_index(name='total')
+polish_pipeline = [
+    {
+        '$match': {'cuisine': 'Polish'}
+    },
+    {
+        '$group': {
+            '_id': '$borough',
+            'total': {'$sum': 1}
+        }
+    }
+]
 
-# Create DF for Italian restaurants
-italian_restaurants = df[df['cuisine'] == 'Italian']
-italian_grouped = italian_restaurants.groupby('borough').size().reset_index(name='total')
+italian_pipeline = [
+    {
+        '$match': {'cuisine': 'Italian'}
+    },
+    {
+        '$group': {
+            '_id': '$borough',
+            'total': {'$sum': 1}
+        }
+    },
+    {
+        '$sort': {'total': -1}
+    }
+]
 
-# Print DF
+# Execution for both tables
+polish_results = list(restaurants_collection.aggregate(polish_pipeline))
+italian_results = list(restaurants_collection.aggregate(italian_pipeline))
+
+# put the results in DataFrames
+polish_df = pd.DataFrame(polish_results)
+italian_df = pd.DataFrame(italian_results)
+
+# Print results
 print("Total number of Polish cuisine restaurants grouped by borough:")
-print(polish_grouped)
+print(polish_df.to_markdown(index=False))
 
-# Sort by big to small
-italian_grouped = italian_grouped.sort_values(by='total', ascending=False)
-
-print("\nTotal number of Italian cuisine restaurants grouped by borough:")
-print(italian_grouped)
+print("\nTotal number of Italian cuisine restaurants grouped by borough (sorted by total):")
+print(italian_df.to_markdown(index=False))
