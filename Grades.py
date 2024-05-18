c99ac36f3df4fdb94d4597889004914ab750a739
Murtaza Haidary <LE1983326@crc-lennox.qc.ca>
2024-05-17 21:29:06 -0400

    grades part1 finished

--- Grades.py
+++ Grades.py
@@ -1,33 +1,54 @@
+from pymongo import MongoClient
 import pandas as pd
 
-# Read the CSV file
-df = pd.read_csv('sample_training.grades.csv')
-
-# dict to save results for each class_id
-class_result = {}
-
-# get class_ids
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['DatabaseLab4']  
+grades_collection = db['Grades']
+# class ids
 class_ids = [149, 350]
 
+# dictionary to save results
+class_result = {}
+
 for class_id in class_ids:
-    class_data = df[df['class_id'] == class_id]
+    pipeline = [
+        {
+            '$match': {'class_id': class_id}
+        },
+        {
+            '$project': {
+                'class_id': 1,
+                'scores': 1
+            }
+        },
+        {
+            '$unwind': '$scores'
+        },
+        {
+            '$group': {
+                '_id': '$scores.type',
+                'avg_score': {'$avg': '$scores.score'}
+            }
+        },
+        {
+            '$project': {
+                '_id': 1,
+                'avg_score': 1
+            }
+        }
+    ]
 
-    class_result[class_id] = []
+    # Execute pipeline
+    result = list(grades_collection.aggregate(pipeline))
 
-    for index, row in class_data.iterrows():
-        for i in range(4):
-            score_type = row[f'scores[{i}].type']
-            score_value = row[f'scores[{i}].score']
-            class_result[class_id].append({'_id': score_type, 'avg_score': score_value})
+    # Convert to DataFrame
+    result_df = pd.DataFrame(result)
 
-    class_result[class_id] = pd.DataFrame(class_result[class_id])
-    # Get avg score by class_id
-    class_result[class_id] = class_result[class_id].groupby('_id')['avg_score'].mean().reset_index()
-
-    # Add class_id field
-    class_result[class_id]['class_id'] = class_id
+    # Save the nswer in class_id
+    class_result[class_id] = result_df
 
 for class_id, result_df in class_result.items():
     print(f"Average score per evaluation type for class_id {class_id}:")
-    print(result_df[['_id', 'avg_score']])
+    print(result_df.to_string(index=False))
     print()
